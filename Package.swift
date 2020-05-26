2405dce7ee2b448508e6c4319b474f3090655c3e
mxgc <mx@great.cat>
2020-05-26 13:14:57 -0700

    add Package.swift for spm compatibility

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Navajo-Swift",
+    products: [
+        .library(
+            name: "Navajo-Swift",
+            targets: ["Navajo-Swift"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "Navajo-Swift",
+            dependencies: [],
+            path: "Source"
+        ),
+    ]
+)
